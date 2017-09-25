0b0798a1ba79067d35b5e69e9e853b0514a6a573
Jaehong Kim <jaehong1007@gmail.com>
2017-09-25 23:50:09 +0900

    blog update

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    # post_list view가 published_date가 존재하는 Post목록만 보여주도록 수정
+
+    posts = Post.objects.filter(published_date__isnull=False)
+    context = {
+        # posts key의 value는 QuerySet
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html', context)
